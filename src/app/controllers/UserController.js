3e056954bbd500bfb8ab9b24dbfaf4d5ff67afd6
gferreira <gferreiraa@gmail.com>
2019-03-09 23:46:13 -0300

    Created: store

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,15 @@
 /* eslint-disable space-before-function-paren */
+const {
+  User
+} = require('../models')
 class UserController {
   create(req, res) {
     return res.render('auth/signup')
+  }
+  async store(req, res) {
+    await User.create(req.body)
+
+    return res.redirect('/')
   }
 }
 module.exports = new UserController()
