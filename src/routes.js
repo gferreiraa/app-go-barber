684a4887c97a216708c7368087065cd894a054a5
gferreira <gferreiraa@gmail.com>
2019-04-02 23:14:11 -0300

    Created Session Controller

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,11 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
 routes.get('/', SessionController.create)
+routes.get('/signin', SessionController.store)
 
 routes.get('/signup', UserController.create)
 routes.post('signup', upload.single('avatar'), UserController.store)
+
+routes.get('/app/dashboard', (req, res) => res.render('dashboard'))
 
 module.exports = routes
